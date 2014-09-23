8c54b0f785473eddb87d2141f0c936ffb2840aaa
Tomasz Dębek <tdebek@Mac-mini-Tomasz.local>
2014-09-23 12:03:53 +0200

    products_controller.rb z @product = Product.new(product_params) na @product = Product.create(product_params) oraz params :avatar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    @product = Product.create(product_params)
 
     respond_to do |format|
       if @product.save
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description)
+      params.require(:product).permit(:title, :description, :avatar)
     end
 end
